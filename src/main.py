fe2fcaa277746025a8d390b4110579dc9f3422e9
Iestyn Parry <29750@bridgend.ac.uk>
2022-03-04 13:24:17 +0000

    directions

--- src/main.py
+++ src/main.py
@@ -6,6 +6,16 @@ def main():
     lives = 3
     print(f"You have {lives} lives remaining")
     
+    direction = input("Where do you want to go? North, South, East or West? ")
+    if direction == "North":
+        print("You went North")
+    elif direction == "South":
+        print("You went South")
+    elif direction == "East":
+        print("You went East")
+    else:
+        print("You went West")
+
 
 main()
     
